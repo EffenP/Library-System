af54f2198f25a5565b547a749e25f09c3a652d39
EthanP <ethanpollard42@gmail.com>
2022-02-08 11:28:26 +0000

    more updates to follow

--- README.md
+++ README.md
@@ -1,3 +1,16 @@
 # Library-System
 
+# todo
 
+- finish adding all functionality (deleting books, editing books state)
+- add form validation
+- use local storage to save current library
+- add modal popup to add book instead of displaying the book form directly
+- tidy up CSS make more readable and approachable for the user
+- refactor code
+
+
+# Future additions
+
+- allow sign in and sign out 
+- use firebase to allow signed in useres to save a library to their account.
--- app.js
+++ app.js
@@ -4,31 +4,31 @@ class Book {
         this.title = title;
         this.author = author;
         this.pages = pages;
-        this.isREad = isRead;
+        this.isRead = isRead;
     }
 }
 
 // UI Class: Handle all the UI tasks
-class uiHandler {
+class UI {
     static displayBooks() {
         const StoredBooks = [
             {
                 title: 'The Hobbit',
                 author: 'J.R.R Tolkien',
                 pages: '400',
-                isRead = 'false'
+                isRead: 'false'
             }, 
             {
                 title: 'Harry Potter and the Philosopher\'s Stone',
                 author: 'J.K Rowling',
                 pages: '600',
-                isRead = 'true'
+                isRead: 'true'
             }
         ];
 
         const books = StoredBooks;
 
-        books.forEach((book) => {uiHandler.addBookToList()})
+        books.forEach((book) => UI.addBookToList(book))
     }
 
     static addBookToList(book) {
@@ -39,13 +39,58 @@ class uiHandler {
         row.innerHTML = `<td>${book.title}</td>
                         <td>${book.author}</td>
                         <td>${book.pages}</td>
-                        <td>${book.isRead}</td>`    }
+                        <td>${book.isRead}</td>
+                        <td> <a href="#" class="btn btn-danger btn-sm delete-btn">X</a></td>
+                        `;
+
+        list.appendChild(row);
+    }
+
+    static resetForm() {
+        document.querySelector('#title').value = '';
+        document.querySelector('#author').value = '';
+        document.querySelector('#pages').value = '';
+        document.querySelector('#isRead').value = 'false';
+        
+    }
+
 }
 
 // Storage Class: handle Local and eventually database storage
 
-// Event: Display Books
+// EVENT: DISPLAY BOOKS
+document.addEventListener('DOMContentLoaded', UI.displayBooks);
 
-// Event: Add a Book
+
+// EVENT: ADD A NEW BOOK
+document.querySelector('#book-form').addEventListener('submit', (e) => {
+    // prevent actual submit
+
+    e.preventDefault(); 
+
+    //get form values
+    const title = document.querySelector('#title').value;
+    const author = document.querySelector('#author').value;
+    const pages = document.querySelector('#pages').value;
+    const isRead = document.querySelector('#isRead').checked;
+
+    // INSTANTIATE THE NEW BOOK
+
+    const book = new Book(title, author, pages, isRead)
+
+    //console.log(book);
+
+    // ADD BOOK TO UI  
+
+    UI.addBookToList(book);
+
+    UI.resetForm();
+
+    // add to local storage
+    
+})
 
 // Event Remove a Book
+
+
+
