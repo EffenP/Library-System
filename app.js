35ce0bd2c5129ccd8cdeec96b831aff44a1563c3
EthanP <ethanpollard42@gmail.com>
2022-02-08 14:14:28 +0000

    added local storage functionality, aswell as the deleting of books stored locally

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 // Book Class: 
 class Book {
     constructor(title, author, pages, isRead){
+        this.id = getBookId();
         this.title = title;
         this.author = author;
         this.pages = pages;
@@ -11,22 +12,7 @@ class Book {
 // UI Class: Handle all the UI tasks
 class UI {
     static displayBooks() {
-        const StoredBooks = [
-            {
-                title: 'The Hobbit',
-                author: 'J.R.R Tolkien',
-                pages: '400',
-                isRead: 'false'
-            }, 
-            {
-                title: 'Harry Potter and the Philosopher\'s Stone',
-                author: 'J.K Rowling',
-                pages: '600',
-                isRead: 'true'
-            }
-        ];
-
-        const books = StoredBooks;
+        const books = Store.getBooks();
 
         books.forEach((book) => UI.addBookToList(book))
     }
@@ -40,10 +26,27 @@ class UI {
                         <td>${book.author}</td>
                         <td>${book.pages}</td>
                         <td>${book.isRead}</td>
+                        <td>${book.id}</td>
                         <td> <a href="#" class="btn btn-danger btn-sm delete-btn">X</a></td>
                         `;
 
         list.appendChild(row);
+    }
+
+    static deleteBook(element) {
+        if(element.classList.contains('delete-btn')) {
+            element.parentElement.parentElement.remove();
+        }
+    }
+
+    static showAlert(message, className) {
+        const div = document.createElement('div');
+        div.className = `alert alert-${className}`;
+        div.appendChild(document.createTextNode(message));
+        const container = document.querySelector('.container');
+        const form = document.querySelector('#book-form');
+        container.insertBefore(div, form);
+        setTimeout(() => document.querySelector('.alert').remove(), 2000);
     }
 
     static resetForm() {
@@ -57,6 +60,37 @@ class UI {
 }
 
 // Storage Class: handle Local and eventually database storage
+class Store {
+    static getBooks() {
+        let books; 
+        if(localStorage.getItem('books') === null) {
+            books = [];
+        } else {
+            books = JSON.parse(localStorage.getItem('books'));
+        }
+        return books;
+    }
+
+    static addBook(book) {
+        const books = Store.getBooks();
+        books.push(book);
+        localStorage.setItem('books', JSON.stringify(books));
+    }
+
+    static removeBook(id) {
+        const books = Store.getBooks();
+
+        books.forEach((book, index) => {
+           if(book.id === parseInt(id)) {
+               books.splice(index, 1);
+           }
+        });
+        localStorage.setItem('books', JSON.stringify(books));
+    }
+}
+
+
+
 
 // EVENT: DISPLAY BOOKS
 document.addEventListener('DOMContentLoaded', UI.displayBooks);
@@ -74,23 +108,46 @@ document.querySelector('#book-form').addEventListener('submit', (e) => {
     const pages = document.querySelector('#pages').value;
     const isRead = document.querySelector('#isRead').checked;
 
-    // INSTANTIATE THE NEW BOOK
+    // VALIDATION
 
-    const book = new Book(title, author, pages, isRead)
+    if(title === '' || author === '' || pages === '') {
+        UI.showAlert('Please Fill in all Fields', 'danger');
+    } else {
+        // INSTANTIATE THE NEW BOOK
 
-    //console.log(book);
+        const book = new Book(title, author, pages, isRead)
+        //console.log(book);
+        // ADD BOOK TO UI  
+        UI.addBookToList(book);
 
-    // ADD BOOK TO UI  
 
-    UI.addBookToList(book);
+        //Store book locally
+        Store.addBook(book);
 
-    UI.resetForm();
+        // SHOW SUCCESSFUL ALERT
+        UI.showAlert('Book Successfully Added', 'success');
 
-    // add to local storage
-    
-})
+        // RESET THE FORM FIELDS
+        UI.resetForm();       
+    }
+});
 
 // Event Remove a Book
+document.querySelector('#book-list').addEventListener('click' , (e) => {
+    // Remove book from the UI
+    UI.deleteBook(e.target);
+    
+    //Remove Book from Local Storage
+    Store.removeBook(e.target.parentElement.previousElementSibling.textContent);
+
+
+    // SHOW SUCCESSFUL ALERT
+    UI.showAlert('Book Deleted', 'success');
+})
 
 
 
+function getBookId() {
+    return parseInt(Math.ceil(Math.random() * Date.now()).toPrecision(16).toString().replace(".", ""))
+
+}
